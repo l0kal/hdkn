78117ea90b66454c823b8dbd67c94b9fe23d418a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-03-26 23:15:08 +0100

    Added a basic API action for installing MSI packages. This should not be trusted as it we want control of the source. Next step is to download them from a specified release API url.

---
+++ src/Plugins/HdknPlugins.AutoUpdate/Http/Api/SourceUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Http;
+using System.Diagnostics;
+
+namespace HdknPlugins.AutoUpdate.Http.Api
+{
+    [ApiAction("autoupdate-sourceupdate")]
+    public class SourceUpdate : ApiAction
+    {
+        public override ActionResult Execute()
+        {
+            var url = BindModel<string>();
+
+            var p = new Process();
+            p.StartInfo.FileName = "msiexec";
+            p.StartInfo.Arguments = String.Format("/i {0} /quiet /l*v {1}", url, "%PROGRAMDATA%\\Hadouken\\AutoUpdater\\" + Guid.NewGuid() + ".txt");
+            p.StartInfo.Verb = "runas";
+
+            var started = p.Start();
+
+            return Json(started);
+        }
+    }
+}
