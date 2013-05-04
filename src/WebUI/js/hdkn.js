cfce0ae020fd82253ab56634d2f3a4fc7cb50734
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-04 16:10:21 +0200

    Added a Hadouken class for simplifying some things like getting plugins.

--- src/WebUI/js/hdkn.js
+++ src/WebUI/js/hdkn.js
@@ -1,60 +1,13 @@
-$(document).ready(function() {
+var Hadouken = new Class({
+  Implements: [ Options, Events ],
 
-  UI.init();
+  initialize: function(network) {
+    this.network = network;
+  },
 
-  var fileData = [];
-
-  jQuery.event.props.push('dataTransfer');
-
-  $('#torrentsDropTarget').bind('drop', function(e){
-    e.stopPropagation();
-    e.preventDefault();
-    
-    var files = e.dataTransfer.files;
-
-    $.each(files, function(index, file) {
-        var fileReader = new FileReader();
-        fileReader.onload = (function(f) {
-          return function(ev) {
-            fileData.push({
-                name: f.name,
-                data: this.result.split(",")[1]
-            });
-
-            console.log(this);
-            $("#torrentsToAdd").append($("<li></li>").text(f.name));
-          };
-        })(files[index]);
-
-        fileReader.readAsDataURL(file);
+  getPlugins: function(callback){
+    this.network.get("/api/plugins", function(data) {
+      callback(data);
     });
-  });
-
-  $("#addtorrents_btn").bind("click", function() {
-    $.each(fileData, function(index, fd) {
-      var d = {
-        data: fileData[index].data
-      };
-
-      $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: "/api/torrents",
-        data: JSON.stringify(d),
-        success: function() {
-            console.log("success!");
-        }
-      })
-    });
-  });
-
-  $('#torrentsDropTarget').bind('dragenter', function(e) {
-    $(this).addClass("active");
-    return false;
-  });
-
-  $('#torrentsDropTarget').bind('dragleave', function(e) {
-    $(this).removeClass("active");
-    return false;
-  });
-});
+  }
+})
