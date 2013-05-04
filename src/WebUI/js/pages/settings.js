875821e4a704a7ff01710c27d62c624bc5e0547b
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-04 17:14:33 +0200

    Setting panes are now static in the HTML file.

--- src/WebUI/js/pages/settings.js
+++ src/WebUI/js/pages/settings.js
@@ -1,3 +1,9 @@
+var SettingType = {
+    "Boolean": 0,
+    "Integer": 1,
+    "String": 2
+};
+
 var Settings = new Class({
     Extends: Page,
 
@@ -12,6 +18,23 @@ var Settings = new Class({
         this.hdkn.listPlugins(function(plugins) {
             this.listPlugins(plugins);
         }.bind(this));
+
+        this.hdkn.listSettings(function(settings) {
+            this.listSettings(settings);
+        }.bind(this));
+
+        var that = this;
+
+        this.element.getElements(".setlink").addEvent('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+
+            that.element.getElements(".setlink").getParent().removeClass("active");
+            this.getParent().addClass("active");
+
+            that.element.getElements(".settings-pane").addClass("hide");
+            that.element.getElementById("pane-" + this.get("href").substring(1)).removeClass("hide");
+        });
     },
 
     listPlugins: function(plugins) {
@@ -36,6 +59,17 @@ var Settings = new Class({
 
             a.inject(li);
             li.inject(pluginHeader, 'after');
+        }
+    },
+
+    listSettings: function(settings) {
+        for(var i = 0; i < settings.length; i++){
+            var setting = settings[i];
+            var element = $$('input[name=' + setting.key + ']');
+
+            if(element != null) {
+                element.set('value', setting.value);
+            }
         }
     }
 });
