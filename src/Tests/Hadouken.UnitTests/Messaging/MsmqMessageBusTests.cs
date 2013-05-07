a54466e78b0a0b78a14d73abb6661863e68b9f68
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-07 10:34:48 +0200

    Added MsmqMessageBusTests which tests the ability to send and receive messages.

---
+++ src/Tests/Hadouken.UnitTests/Messaging/MsmqMessageBusTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Common.DI.Ninject;
+using NUnit.Framework;
+using Hadouken.Common.Messaging.Msmq;
+using Hadouken.Common;
+using System.Threading;
+
+namespace Hadouken.UnitTests.Messaging
+{
+    public class MsmqMessageBusTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Kernel.SetResolver(new NinjectDependencyResolver());
+        }
+
+
+        [Test]
+        public void Can_send_and_receive()
+        {
+            var bus = new MsmqMessageBus("msmq://localhost/hdkn_test");
+            bus.Load();
+
+            var mre = new ManualResetEvent(false);
+
+            bus.Subscribe<TestMessage>(_ => mre.Set());
+            bus.Publish(new TestMessage {Text = "Test message"});
+
+            Assert.IsTrue(mre.WaitOne());
+        }
+    }
+}
---
+++ src/Tests/Hadouken.UnitTests/Messaging/TestMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hadouken.Common.Messaging;
+
+namespace Hadouken.UnitTests.Messaging
+{
+    public class TestMessage : Message
+    {
+        public string Text { get; set; }
+    }
+}
