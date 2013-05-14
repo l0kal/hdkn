135cc0405a28091793373fcf0976936dcbfb2439
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-14 18:35:13 +0200

    Added TorrentFilesController which lists files for a specific torrent.

---
+++ src/Main/Hadouken.Http.Api/TorrentFilesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Http;
+using Hadouken.BitTorrent;
+using System.Net.Http;
+
+namespace Hadouken.Http.Api
+{
+    public class TorrentFilesController : ApiController
+    {
+        private readonly IBitTorrentEngine _torrentEngine;
+
+        public TorrentFilesController(IBitTorrentEngine torrentEngine)
+        {
+            _torrentEngine = torrentEngine;
+        }
+
+        public object Get(string id)
+        {
+            if (_torrentEngine.Managers.ContainsKey(id))
+            {
+                return new
+                    {
+                        files = new object[]
+                            {
+                                id,
+                                (from f in _torrentEngine.Managers[id].Torrent.Files
+                                 select new object[]
+                                     {
+                                         f.Path,
+                                         f.Length,
+                                         f.BytesDownloaded,
+                                         f.Priority
+                                     })
+                            }
+                    };
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.NotFound);
+        }
+    }
+}
