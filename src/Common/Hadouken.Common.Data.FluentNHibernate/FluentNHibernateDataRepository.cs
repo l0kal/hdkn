69c5c27a776f93adf76ce4a0329a71353f03a440
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-14 20:08:00 +0200

    Added locks to the data repository.

--- src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
+++ src/Common/Hadouken.Common.Data.FluentNHibernate/FluentNHibernateDataRepository.cs
@@ -18,6 +18,8 @@ namespace Hadouken.Common.Data.FluentNHibernate
         private static System.Data.SQLite.SQLiteConnection __conn__;
 #pragma warning restore 0169
 
+        private readonly object _lock = new object();
+
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
         private ISessionFactory _sessionFactory;
@@ -46,47 +48,74 @@ namespace Hadouken.Common.Data.FluentNHibernate
 
             Logger.Debug("Opening an ISession to use.");
 
-            _session = _sessionFactory.OpenSession();
+            lock (_lock)
+            {
+                _session = _sessionFactory.OpenSession();                
+            }
         }
 
         public void Save<TModel>(TModel model) where TModel : Model, new()
         {
-            _session.Save(model);
+            lock (_lock)
+            {
+                _session.Save(model);
+            }
         }
 
         public void SaveOrUpdate<TModel>(TModel model) where TModel : Model, new()
         {
-            _session.SaveOrUpdate(model);
+            lock (_lock)
+            {
+                _session.SaveOrUpdate(model);
+            }
         }
 
         public void Update<TModel>(TModel model) where TModel : Model, new()
         {
-            _session.Update(model);
+            lock (_lock)
+            {
+                _session.Update(model);
+            }
         }
 
         public void Delete<TModel>(TModel model) where TModel : Model, new()
         {
-            _session.Delete(model);
+            lock (_lock)
+            {
+                _session.Delete(model);
+            }
         }
 
         public TModel Single<TModel>(int id) where TModel : Model, new()
         {
-            return _session.Get<TModel>(id);
+            lock (_lock)
+            {
+                return _session.Get<TModel>(id);
+            }
         }
 
         public TModel Single<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
         {
-            return _session.Query<TModel>().Where(query).SingleOrDefault();
+            lock (_lock)
+            {
+                return _session.Query<TModel>().Where(query).SingleOrDefault();
+            }
         }
 
         public IEnumerable<TModel> List<TModel>() where TModel : Model, new()
         {
-            return _session.Query<TModel>().ToList();
+            lock (_lock)
+            {
+                return _session.Query<TModel>().ToList();
+            }
         }
 
         public IEnumerable<TModel> List<TModel>(System.Linq.Expressions.Expression<Func<TModel, bool>> query) where TModel : Model, new()
         {
-            return _session.Query<TModel>().Where(query).ToList();
+            lock (_lock)
+            {
+                return _session.Query<TModel>().Where(query).ToList();
+            }
         }
     }
 }
