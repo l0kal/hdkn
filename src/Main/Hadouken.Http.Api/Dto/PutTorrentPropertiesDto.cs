a0f1c18ef28efc3142a24e5374f867949b9903bb
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-16 16:17:46 +0200

    Added beginning of the TorrentPropertiesController.

---
+++ src/Main/Hadouken.Http.Api/Dto/PutTorrentPropertiesDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Http.Api.Dto
+{
+    public class PutTorrentPropertiesDto
+    {
+        public string Label { get; set; }
+    }
+}
---
+++ src/Main/Hadouken.Http.Api/TorrentPropertiesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Http;
+using Hadouken.BitTorrent;
+using System.Net.Http;
+using Hadouken.Http.Api.Dto;
+
+namespace Hadouken.Http.Api
+{
+    public class TorrentPropertiesController : ApiController
+    {
+        private readonly IBitTorrentEngine _torrentEngine;
+
+        public TorrentPropertiesController(IBitTorrentEngine torrentEngine)
+        {
+            _torrentEngine = torrentEngine;
+        }
+
+        public object Get(string[] id = null)
+        {
+            if (id == null || id.Length == 0)
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            return null;
+        }
+
+        public HttpResponseMessage Put([FromBody] PutTorrentPropertiesDto dto, [FromUri] string[] id = null)
+        {
+            return null;
+        }
+    }
+}
