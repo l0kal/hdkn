5c1693aee393db541b64e24ace9dbb75e47d881a
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-17 16:18:59 +0200

    Fixed the FoldersController.

--- src/Plugins/HdknPlugins.AutoAdd/Http/Api/FoldersController.cs
+++ src/Plugins/HdknPlugins.AutoAdd/Http/Api/FoldersController.cs
@@ -1,15 +1,93 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using HdknPlugins.AutoAdd.Http.Dto;
+using Hadouken.Common.Data;
+using HdknPlugins.AutoAdd.Data.Models;
+using System.Net.Http;
+using System.Globalization;
 
 namespace HdknPlugins.AutoAdd.Http.Api
 {
     public class FoldersController : ApiController
     {
-        public object Get()
+        private readonly IDataRepository _repository;
+
+        public FoldersController(IDataRepository repository)
         {
-            return new
+            _repository = repository;
+        }
+
+        public IEnumerable<FolderDto> Get()
+        {
+            return (from folder in _repository.List<Folder>()
+                    select new FolderDto
+                        {
+                            Id = folder.Id,
+                            AutoStart = folder.AutoStart,
+                            ExcludeFilter = folder.ExcludeFilter,
+                            IncludeFilter = folder.IncludeFilter,
+                            Label = folder.Label,
+                            Path = folder.Path
+                        });
+        }
+
+        public HttpResponseMessage Post([FromBody] FolderDto dto)
+        {
+            var f = new Folder
                 {
-                    folders = new object[] {}
+                    AutoStart = (dto.AutoStart ?? false),
+                    ExcludeFilter = dto.ExcludeFilter,
+                    IncludeFilter = dto.IncludeFilter,
+                    Label = dto.Label,
+                    Path = dto.Path
                 };
+
+            _repository.Save(f);
+
+            using (var response = Request.CreateResponse(HttpStatusCode.Created))
+            {
+                response.Headers.Location = new Uri(Request.RequestUri, f.Id.ToString(CultureInfo.InvariantCulture));
+                return response;
+            }
+        }
+
+        public HttpResponseMessage Put([FromUri] int id, [FromBody] FolderDto dto)
+        {
+            var f = _repository.Single<Folder>(id);
+
+            if (f == null)
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            
+            f.AutoStart = (dto.AutoStart ?? f.AutoStart);
+            f.ExcludeFilter = (dto.ExcludeFilter ?? f.ExcludeFilter);
+            f.IncludeFilter = (dto.IncludeFilter ?? f.IncludeFilter);
+            f.Label = (dto.Label ?? f.Label);
+            f.Path = (dto.Path ?? f.Path);
+
+            _repository.Update(f);
+
+            using (var response = Request.CreateResponse(HttpStatusCode.NoContent))
+            {
+                return response;
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            var f = _repository.Single<Folder>(id);
+
+            if (f == null)
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+
+            _repository.Delete(f);
+
+            using (var response = Request.CreateResponse(HttpStatusCode.NoContent))
+            {
+                return response;
+            }
         }
     }
 }
---
+++ src/Plugins/HdknPlugins.AutoAdd/Http/Dto/FolderDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HdknPlugins.AutoAdd.Http.Dto
+{
+    public class FolderDto
+    {
+        public int? Id { get; set; }
+        public string Path { get; set; }
+        public string Label { get; set; }
+        public string ExcludeFilter { get; set; }
+        public string IncludeFilter { get; set; }
+        public bool? AutoStart { get; set; }
+    }
+}
