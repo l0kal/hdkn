b6ad9053edf812d5bbc0cd539301d6ec13458a73
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-27 16:01:18 +0200

    Added a few base interfaces and a DTO for Torrent.

---
+++ src/Main/Hadouken/Events/BitTorrent/ITorrentEventListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Events.BitTorrent
+{
+    public interface ITorrentEventListener : IEventListener
+    {
+        void OnAdded(Action<Torrent> callback);
+        void OnRemoved(Action<Torrent> callback);
+    }
+}
---
+++ src/Main/Hadouken/Events/BitTorrent/ITorrentEventPublisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Events.BitTorrent
+{
+    public interface ITorrentEventPublisher : IEventPublisher
+    {
+        void PublishTorrentAdded(Torrent torrent);
+        void PublishTorrentRemoved(Torrent torrent);
+    }
+}
