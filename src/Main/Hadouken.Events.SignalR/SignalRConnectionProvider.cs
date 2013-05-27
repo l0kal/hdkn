36dadb54324f2781376ede3ff60204080ec9c4e9
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-27 17:21:54 +0200

    Added a SignalRConnectionProvider.

---
+++ src/Main/Hadouken.Events.SignalR/SignalRConnectionProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Hadouken.Configuration;
+
+namespace Hadouken.Events.SignalR
+{
+    [Component(ComponentLifestyle.Singleton)]
+    public class SignalRConnectionProvider : ISignalRConnectionProvider
+    {
+        private IHubConnection _connection;
+
+        private IHubConnection Create()
+        {
+            return new HubConnection("");
+        }
+
+        public IHubConnection GetConnection()
+        {
+            return (_connection ?? (_connection = Create()));
+        }
+    }
+}
