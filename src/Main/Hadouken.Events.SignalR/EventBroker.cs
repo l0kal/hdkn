9fb9106b3ffbf62c45132b33aaeb455cc16c1bf9
Viktor Elofsson <viktor@viktorelofsson.se>
2013-05-28 16:26:27 +0200

    Added IEventBroker interface which should host the SignalR host.

---
+++ src/Main/Hadouken.Events.SignalR/EventBroker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin.Host.HttpListener;
+
+namespace Hadouken.Events.SignalR
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var cfg = new HubConfiguration
+                {
+
+                };
+
+            app.MapHubs("/hubs", cfg);
+        }
+    }
+
+    [Component(ComponentLifestyle.Singleton)]
+    public class EventBroker : IEventBroker
+    {
+#pragma warning disable 0169
+        private static readonly Microsoft.Owin.Host.HttpListener.OwinHttpListener __owl;
+#pragma warning restore 0169
+
+        private readonly ManualResetEvent _resetEvent = new ManualResetEvent(false);
+
+        public void Start()
+        {
+            Task.Factory.StartNew(() =>
+                {
+                    using (WebApplication.Start<Startup>("url"))
+                    {
+                        _resetEvent.WaitOne();
+                    }
+                });
+        }
+
+        public void Stop()
+        {
+            _resetEvent.Set();
+        }
+    }
+}
---
+++ src/Main/Hadouken/Events/IEventBroker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hadouken.Events
+{
+    public interface IEventBroker
+    {
+        void Start();
+        void Stop();
+    }
+}
